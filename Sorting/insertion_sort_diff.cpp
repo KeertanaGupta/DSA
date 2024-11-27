a1925098c4af279b5204ede0658f1f288c5c775c
Keertana Gupta <keertanagupta@gmail.com>
2024-11-27 23:31:51 +0530

    dsa updated

--- Sorting/insertion_sort_diff.cpp
+++ Sorting/insertion_sort_diff.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int main(){
     int n;
@@ -8,17 +7,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=1;i<n;i++){
-        int curr = arr[i];
-        int j=i-1;
-        while(j>=0 && curr<arr[j]){
-            arr[j+1] = arr[j];
+    for(int i=0;i<n;i++){
+        int j = i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
             j--;
         }
-
-        arr[j+1] = curr;
     }
-    cout<<"Sorted array: ";
+
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
