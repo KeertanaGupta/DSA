eb33df81bba5e2f039d78a41e3f1c7d3fa4a7c0d
Keertana Gupta <keertanagupta@gmail.com>
2024-12-02 02:23:13 +0530

    updated

--- Array-1/RotateAnyDir.cpp
+++ Array-1/RotateAnyDir.cpp
@@ -10,9 +10,20 @@ void Reverse(int arr[],int start, int end){
     }
 }
 void RotateToRight(int arr[], int n, int k){
-    Reverse(arr,0,n-k-1);
-    Reverse(arr,n-k,n-1);
-    Reverse(arr,0,n-1);
+    // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+void RotateToLeft(int arr[], int n, int k){
+  // Reverse first k elements
+  Reverse(arr, 0, k - 1);
+  // Reverse last n-k elements
+  Reverse(arr, k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
 }
 int main(){
     int n;
@@ -27,7 +38,21 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    RotateToRight(arr, n, k);
+    int ch;
+    cout<<"Enter 1-->Right"<<endl<<"      2-->Left"<<endl;
+    cin>>ch;
+    switch (ch)
+    {
+    case 1:
+            RotateToRight(arr, n, k);
+            break;
+    case 2: 
+            RotateToLeft(arr, n, k);
+            break;
+    default:
+            cout<<"Invalid Choice"<<endl;
+            break;
+    }
     cout << "Array after "<<k<<" rotations: ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
