f092ab198d28ada7b048c2637848339d7b951ac3
Keertana Gupta <keertanagupta@gmail.com>
2024-12-14 21:44:03 +0530

    updated

---
+++ Arrays_all_level/SubArray_new.cpp/longSumK_hash.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    int N,k;
+    cin>>N>>k;
+    int arr[N];
+    for(int i=0;i<N;i++){
+      cin>>arr[i];
+    }
+    return 0;
+}
